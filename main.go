b8ac550bceb1f860c9f7e29aa4b03e9f5749ac3f
AntoninHuaut <me@antoninhuaut.fr>
2025-02-23 23:01:01 +0100

    feat: dungeons

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"DofusNoobsIdentifierOffline/boot"
+	"DofusNoobsIdentifierOffline/domain"
 	"DofusNoobsIdentifierOffline/internal"
 	"fmt"
 	"log"
-	"sort"
+	"slices"
 	"strconv"
 	"strings"
+)
+
+const (
+	questsKey   = "quests"
+	dungeonsKey = "dungeons"
 )
 
 func main() {
@@ -19,11 +25,17 @@ func main() {
 	}
 	fmt.Println("Total urls:", len(sitemap.Urls))
 
-	quests, err := boot.LoadQuests()
+	quests, err := boot.LoadDofusDbResources[domain.DofusDbQuestLight](questsKey, domain.QuestsFile, boot.HttpClient.RequestDofusApiAllQuests)
 	if err != nil {
 		log.Fatalf("LoadQuests: %v", err)
 	}
 	fmt.Println("Total quests:", len(quests.Data))
+
+	dungeons, err := boot.LoadDofusDbResources[domain.DofusDbDungeonLight](dungeonsKey, domain.DungeonsFile, boot.HttpClient.RequestDofusApiAllDungeons)
+	if err != nil {
+		log.Fatalf("LoadDungeons: %v", err)
+	}
+	fmt.Println("Total dungeons:", len(dungeons.Data))
 
 	titles, err := boot.LoadTitles(sitemap.Urls)
 	if err != nil {
@@ -31,21 +43,38 @@ func main() {
 	}
 	fmt.Println("Total titles:", len(titles))
 
-	var logs []string
-	output := make(map[int]string)
-	for _, quest := range quests.Data {
-		location, similarity, locLog := internal.GetLocationFromTarget(titles, quest)
+	logs := make(map[string][]string)         // map[resourceType][]logs
+	output := make(map[string]map[int]string) // map[resourceType]map[resourceId]dofusNoobsUrl
+	initKey := func(key string) {
+		logs[key] = make([]string, 0)
+		output[key] = make(map[int]string)
+	}
+	initKey(questsKey)
+	initKey(dungeonsKey)
+
+	resolveKey := func(key string, name string, id int) {
+		location, similarity, locLog := internal.GetLocationFromTarget(titles, name)
 
 		similarityNb, convertErr := strconv.ParseFloat(similarity, 64)
 		if locLog != "" && ((convertErr == nil && similarityNb < 0.9) || similarity == "prefixOrSuffix") {
-			logs = append(logs, locLog)
+			logs[key] = append(logs[key], locLog)
 		}
-		output[quest.ID] = location
+		output[key][id] = location
+	}
+	logKey := func(key string) {
+		slices.Sort(logs[key])
+		internal.WriteToFile(fmt.Sprintf("logs_%s.txt", key), strings.Join(logs[key], ""), false, true)
 	}
 
-	sort.Strings(logs)
-	fmt.Println(strings.Join(logs, ""))
-	internal.WriteToFile("logs.txt", strings.Join(logs, ""), false, true)
+	for _, quest := range quests.Data {
+		resolveKey(questsKey, quest.Name["fr"], quest.ID)
+	}
+	logKey(questsKey)
+
+	for _, dungeon := range dungeons.Data {
+		resolveKey(dungeonsKey, dungeon.Name["fr"], dungeon.ID)
+	}
+	logKey(dungeonsKey)
 
 	internal.WriteToFile("mapping.json", output, false, false)
 	internal.WriteToFile("mapping_formatted.json", output, true, false)
